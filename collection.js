2f29875a0f4c8286dfac1b9dd87e6f9bbdac308c
ayoubelyazizi <ayoube.elyazizi@gmail.com>
2020-11-10 16:59:49 +0100

    ajout de js

---
+++ collection.js
@@ -0,0 +1,12 @@
+const collection = [{
+        title: 'Prison Break',
+        author: 'Paul T. Scheuring',
+        category: ['action'],
+        link: 'https://www.youtube.com/watch?v=1ZMNujDjGD4',
+        description: 'The plot of Arrested Development revolves around the members of the Bluth family, a formerly wealthy family who continue to lead extravagant lifestyles despite their changed circumstances and who often find themselves drawn into interactions with incestuous undertones.'
+    },
+    {
+
+
+    }
+]
