559a9cedde70b9aa374ace8335f1cb9be5dbc740
ayoubelyazizi <ayoube.elyazizi@gmail.com>
2020-11-12 09:56:27 +0100

    collection.js complet

--- collection.js
+++ collection.js
@@ -1,12 +1,82 @@
-const collection = [{
+const collection = [
+    {
         title: 'Prison Break',
         author: 'Paul T. Scheuring',
         category: ['action'],
-        link: 'https://www.youtube.com/watch?v=1ZMNujDjGD4',
-        description: 'The plot of Arrested Development revolves around the members of the Bluth family, a formerly wealthy family who continue to lead extravagant lifestyles despite their changed circumstances and who often find themselves drawn into interactions with incestuous undertones.'
+        link: 'https://youtu.be/AL9zLctDJaU',
+        description: "En raison d'un complot politique, un innocent est condamné à mort et son seul espoir est son frère, qui a pour mission de se faire envoyer délibérément dans la même prison pour qu'ils puissent s'en échapper ensemble."
     },
+
     {
+        title: 'Casa de Papel',
+        author: 'Alex Pina',
+        category: ['action'],
+        link: 'https://youtu.be/hMANIarjT50',
+        description: "Un groupe de voleurs très particuliers s'attaquent à l'usine de Moneda et Timbre pour mener le braquage le plus parfait de l'histoire de l'Espagne et repartir avec 2400 millions d'euros."
+    },
 
+    {
+        title: 'Family Business',
+        author: 'Igor Gotesman',
+        category: ['comedie'],
+        link: 'https://youtu.be/axYAgIVfox4',
+        description: "Apprenant que le cannabis sera légalisé, Joseph, un entrepreneur en faillite décide avec l'aide de sa famille et de ses amis de transformer la boucherie casher de son père et d'ouvrir le premier coffee shop de marijuana en France."
+    },
 
+    {
+        title: 'Friends',
+        author: 'David Crane',
+        category: ['comedie'],
+        link: 'https://youtu.be/hDNNmeeJs1Q',
+        description: "Suit les vies personnelles et professionnelles de six amis d'une vingtaine et trentaine d'années vivant à Manhattan."
+    },
+
+    {
+        title: 'Suits',
+        author: 'Aaron Korsh',
+        category: ['drama'],
+        link: 'https://youtu.be/85z53bAebsI',
+        description: " Le brillant Mike Ross, ayant abandonné ses études universitaires, est en cavale à la suite d'un trafic de drogue qui mal tourné. Il trouve un emploi où il travaille avec Harvey Specter, l'un des meilleurs avocats de la ville de New York."
+    },
+
+    {
+        title: 'Lucifer',
+        author: 'Tom Kapinos',
+        category: ['fantasy'],
+        link: 'https://youtu.be/X4bF_quwNtw',
+        description: "Lucifer Morningstar en a assez d'être serviteur dévoué en Enfer et décide de passer du temps sur Terre afin de mieux comprendre l'humanité. Il s'installe à Los Angeles, la cité des anges."
+    },
+
+    {
+        title: 'Black Mirror',
+        author: 'Charlie Brooker',
+        category: ['Sci-Fi'],
+        link: 'https://youtu.be/jDiYGjp5iFg',
+        description: "Une série d'anthologie explorant un monde tourné vers la haute technologie où se heurtent les plus grandes innovations et les instincts les plus sombres de l'humanité."
+    },
+
+    {
+        title: 'Narcos',
+        author: 'Carlo Bernanrd',
+        category: ['biographie'],
+        link: 'https://youtu.be/xl8zdCY-abw',
+        description: "Un article sur les exploits criminels du seigneur colombien de la drogue, Pablo Escobar, ainsi que sur les nombreux autres grands noms de la drogue qui ont frappé le pays au fil des ans."
+    },
+
+    {
+        title: 'Breaking Bad',
+        author: 'Vince Gillingan',
+        category: ['crime'],
+        link : 'https://youtu.be/HhesaQXLuRY',
+        description: "Un professeur de chimie de lycée chez qui on a diagnostiqué un cancer du poumon inopérable se tourne vers la fabrication et la vente de méthamphétamine pour assurer l'avenir de sa famille."
+    },
+
+    {
+        title: 'The Boys',
+        author: 'Eric Kripke',
+        category: ['comedie'],
+        link : 'https://youtu.be/06rueu_fh30',
+        description: "Une histoire d'action centrée sur une équipe de la CIA chargée de maintenir les super-héros en ligne, par tous les moyens nécessaires"
     }
+
 ]
