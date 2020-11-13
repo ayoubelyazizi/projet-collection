3b0270d77e601214679b5d227c1ba75a162c720f
ayoubelyazizi <ayoube.elyazizi@gmail.com>
2020-11-13 16:32:44 +0100

    recherche en js ok

--- collection.js
+++ collection.js
@@ -1,5 +1,4 @@
-const collection = [
-    {
+const collection = [{
         title: 'Prison Break ',
         img: 'https://images6.fanpop.com/image/photos/34800000/Prison-Break-prison-break-34829728-1280-960.jpg',
         author: 'Paul T. Scheuring',
@@ -76,7 +75,7 @@ const collection = [
         img: 'https://wallpaperstock.net/breaking-bad%2C--series%2C-american_wallpapers_57504_1280x960.jpg',
         author: 'Vince Gillingan',
         category: ['Crime'],
-        link : 'https://youtu.be/HhesaQXLuRY',
+        link: 'https://youtu.be/HhesaQXLuRY',
         description: "Un professeur de chimie de lycée chez qui on a diagnostiqué un cancer du poumon inopérable se tourne vers la fabrication et la vente de méthamphétamine pour assurer l'avenir de sa famille."
     },
 
@@ -85,7 +84,7 @@ const collection = [
         img: 'https://www.fredzone.org/wp-content/uploads/2020/09/The-Boys-Amazon.jpg',
         author: 'Eric Kripke',
         category: ['Comedie'],
-        link : 'https://youtu.be/06rueu_fh30',
+        link: 'https://youtu.be/06rueu_fh30',
         description: "Une histoire d'action centrée sur une équipe de la CIA chargée de maintenir les super-héros en ligne, par tous les moyens nécessaires"
     }
 
@@ -99,10 +98,7 @@ columns.className = 'columns is-multiline';
 
 mainContainer.appendChild(columns)
 
-
-for (let element of collection){
-
-
+for (let element of collection) {
 
     const column = document.createElement('div')
     column.className = 'column is-3';
@@ -120,38 +116,38 @@ for (let element of collection){
     img.src = element.img;
 
     const card_content = document.createElement('div')
-    card_content.className= 'card-content';
+    card_content.className = 'card-content';
 
     const tag = document.createElement('span')
-    tag.className= 'tag is-success'
-    tag.textContent= element.category;
+    tag.className = 'tag is-success'
+    tag.textContent = element.category;
 
     const media = document.createElement('div')
     media.className = 'media'
 
     const media_content = document.createElement('div')
-    media_content.className= 'media-content'
+    media_content.className = 'media-content'
 
     const title = document.createElement('p')
     title.className = 'title is-4'
     title.textContent = element.title;
 
     const link = document.createElement('a')
-    link.target= '_blank'
+    link.target = '_blank'
     link.href = element.link;
 
     const icon_yt = document.createElement('i')
     icon_yt.className = 'fab fa-youtube fa-1x'
-    
+
     const subtitle = document.createElement('p')
     subtitle.className = 'subtitle is-6'
     subtitle.textContent = element.author;
-    
+
     const content = document.createElement('div')
     content.className = 'content'
     content.textContent = element.description;
 
-   
+
     columns.appendChild(column)
     column.appendChild(card)
     card.appendChild(card_image)
@@ -167,7 +163,92 @@ for (let element of collection){
     media_content.appendChild(subtitle)
     card_content.appendChild(content)
 
+}
+
+function filterSeries(pattern) {
+    const results = []
+
+    for (let serie of collection) {
+        if (serie.title.toLowerCase().match(pattern) || serie.description.toLowerCase().match(pattern))
+            results.push(serie)
+    }
+
+    const resultsDiv = document.querySelector('#results')
+    resultsDiv.innerHTML = ''
+    mainContainer.innerHTML = ''
+
+    for (let element of results) {
+
+
+        const column = document.createElement('div')
+        column.className = 'column is-3';
+
+        const card = document.createElement('div')
+        card.className = 'card';
+
+        const card_image = document.createElement('div')
+        card_image.className = 'card-image';
+
+        const image = document.createElement('figure')
+        image.className = 'image is-4by3';
+
+        const img = document.createElement('img')
+        img.src = element.img;
+
+        const card_content = document.createElement('div')
+        card_content.className = 'card-content';
+
+        const tag = document.createElement('span')
+        tag.className = 'tag is-success'
+        tag.textContent = element.category;
+
+        const media = document.createElement('div')
+        media.className = 'media'
+
+        const media_content = document.createElement('div')
+        media_content.className = 'media-content'
+
+        const title = document.createElement('p')
+        title.className = 'title is-4'
+        title.textContent = element.title;
+
+        const link = document.createElement('a')
+        link.target = '_blank'
+        link.href = element.link;
+
+        const icon_yt = document.createElement('i')
+        icon_yt.className = 'fab fa-youtube fa-1x'
+
+        const subtitle = document.createElement('p')
+        subtitle.className = 'subtitle is-6'
+        subtitle.textContent = element.author;
+
+        const content = document.createElement('div')
+        content.className = 'content'
+        content.textContent = element.description;
+
+
+        resultsDiv.appendChild(column)
+        column.appendChild(card)
+        card.appendChild(card_image)
+        card_image.appendChild(image)
+        image.appendChild(img)
+        card.appendChild(card_content)
+        card_content.appendChild(tag)
+        card_content.appendChild(media)
+        media.appendChild(media_content)
+        media_content.appendChild(title)
+        title.appendChild(link)
+        link.appendChild(icon_yt)
+        media_content.appendChild(subtitle)
+        card_content.appendChild(content)
+
+
+    }
 
 }
 
-
+document.querySelector('#filter_input')
+    .addEventListener('keyup', input => {
+        filterSeries(input.target.value.toLowerCase())
+    })
